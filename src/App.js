a8bd61dde9ff6c5a2abf7f88823bd8234465015e
Suresh Melvin Sigera <sureshsigera@gmail.com>
2024-03-06 20:56:28 -0500

    Notes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [joke, setJoke] = useState({});
+
+  const handleSearch = async () => {
+    const response = await fetch(
+      `https://api.chucknorris.io/jokes/random`
+    );
+    const jsonData = await response.json();
+    setJoke(jsonData);
+  };
+
+  // // Use useEffect to call handleSearch on component mount
+  // useEffect(() => {
+  //   handleSearch();
+  // }, []); // Empty dependency array means this runs only on mount
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Get Chuck Norris Joke</h1>
+    <button onClick={handleSearch}>Get Chuck Norris Joke</button>
+    <p>{joke.value}</p>
+    </>
   );
 }
 
